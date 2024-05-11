84390a3221d718823d6726e4ff94523ce384009e
KengMagee <155960435+KengMagee@users.noreply.github.com>
2024-05-11 22:52:46 +1000

    added search city input and updated css

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <h1>Weather Search Engine</h1>
         <Weather />
       </header>
+      <footer>
+        Project by{" "}
+        <a href="https://www.shecodes.io/graduates/102970-karen-felipe">
+          Karen Felipe
+        </a>
+        , open sourced on <a href="">Github</a> and hosted on{" "}
+        <a href="">Netlify</a>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
 
 export default function Weather() {
-  return <h1>Hello from Sydney</h1>;
+  let [city, setCity] = useState("");
+  let [message, setMessage] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setMessage(`It is currently 20°C in ${city}`);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  return (
+    <div>
+      <form className="citySearch" onSubmit={handleSubmit}>
+        <input type="search" placeholder="Type a city" onChange={updateCity} />
+        <input type="submit" value="Search" />
+      </form>
+      <h2>{message}</h2>
+    </div>
+  );
 }
